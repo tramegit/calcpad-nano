17bcc452c495f78743b2184ecf54c81cffee0ba8
Paulo Ormonde <ormonde.paulo@gmail.com>
2024-06-25 02:49:38 -0300

    send first setup.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+wget https://github.com/tramegit/calcpad-nano/blob/main/cpd.nanorc
+mv ./cpd.nanorc /usr/share/nano/cpd.nanorc
+echo -e "\n The file cpd.nanorc was installed with success!"
+
+
